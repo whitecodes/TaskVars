a4b94523d4008a0d9ba775058d5ceff56c4111e3
whitecodes <whitemailg@gmail.com>
2026-03-15 12:34:55 +0800

    清理无用代码，更新.gitignore，移除MyMessageBundle和MyToolWindow，调整plugin.xml和相关资源

--- src/main/kotlin/com/wille/taskvars/CurrentTaskMacro.kt
+++ src/main/kotlin/com/wille/taskvars/CurrentTaskMacro.kt
@@ -6,7 +6,6 @@ import com.intellij.codeInsight.template.macro.MacroBase
 import com.intellij.codeInsight.template.Result
 import com.intellij.codeInsight.template.TextResult
 import com.intellij.openapi.project.Project
-import com.intellij.tasks.Task
 import com.intellij.tasks.TaskManager
 
 /**
--- src/main/kotlin/com/wille/taskvars/MyToolWindow.kt
+++
@@ -1,8 +0,0 @@
-package com.wille.taskvars
-
-// The tool window UI has been removed. This file remains as a placeholder
-// so existing references (if any) continue to compile. All UI has been
-// replaced by a live template macro implementation `CurrentTaskMacro`.
-
-// ...existing code...
-
